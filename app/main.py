0824591465989074e40c77664856c950f317af7a
Adrián Meneses Ramos <amr.academics@gmail.com>
2026-03-13 16:39:38 +0000

    Phase 2 (file metada)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from app.authentication.router import router as authentication_router
+from app.authentication.router import router as auth_router
 from app.files.router import router as files_router
 
 app = FastAPI()
 
-@app.get("/healthcheck")
-async def healthcheck() -> dict[str, str]:
-    return {"status": "ok"}
-
-app.include_router(authentication_router)
-app.include_router(files_router, prefix="/files")
+app.include_router(auth_router, prefix="/authentication", tags=["Authentication"])
+app.include_router(files_router, prefix="/files", tags=["Files"])
